07f8787496a9ab197b5a641ee2f773f89dda43e3
Abhishekt2904 <158027625+Abhishekt2904@users.noreply.github.com>
2024-12-16 16:57:36 +0530

    Uncommented Order Now Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -152,10 +152,10 @@ const Navbar = () => {
            <Link to="/order" className="hidden md:inline-block">
 
 
-{/*             <button className="bg-gradient-to-r from-[#f87709] to-[#f88a12] text-white px-8 py-3 rounded-full shadow-lg transition-transform transform hover:scale-105 flex items-center space-x-2">
+       <button className="bg-gradient-to-r from-[#f87709] to-[#f88a12] text-white px-8 py-3 rounded-full shadow-lg transition-transform transform hover:scale-105 flex items-center space-x-2">
               <span>Order Now</span>
               <MdShoppingCart className="w-6 h-6" />
-            </button> */}
+            </button> 
           </Link>  
         </div>
 
@@ -263,10 +263,10 @@ const Navbar = () => {
               <Link to="/order">
 
 
-{/*                 <button className="bg-gradient-to-r from-[#f87709] to-[#f88a12] text-white px-8 py-3 rounded-full shadow-lg transition-transform transform hover:scale-105 w-full flex justify-center items-center space-x-2">
+           <button className="bg-gradient-to-r from-[#f87709] to-[#f88a12] text-white px-8 py-3 rounded-full shadow-lg transition-transform transform hover:scale-105 w-full flex justify-center items-center space-x-2">
                   <span>Order Now</span>
                   <MdShoppingCart className="w-6 h-6" />
-                </button> */}
+                </button>
 
               </Link> 
             </li> 
