e78dc6caf01a013f7ae69e33c155dbc09f39dc34
Saroj kumar <78174385+sarojkum822@users.noreply.github.com>
2025-02-11 03:37:10 +0530

    Update OurJourney.jsx

--- src/components/OurJourney.jsx
+++ src/components/OurJourney.jsx
@@ -106,7 +106,7 @@ const Timeline = () => {
             contents: "Egg Bucket makes its first sale of fresh eggs.",
           },
           {
-            year: "June 2023",
+            year: "June 2022",
             goal: "Market Research Completion",
             contents:
               "Egg Bucket completes initial market research and finalizes its business plan.",
@@ -153,13 +153,13 @@ const Timeline = () => {
               "Implements quality control measures to ensure the highest product standards.",
           },
           {
-            year: "March 2023",
+            year: "March 2024",
             goal: "Annual Returns Filing",
             contents:
               "Kacklewalls Nutrition Private Limited files its provisional Annual Returns and Financial Statements.",
           },
           {
-            year: "March 2023 - Present",
+            year: "March 2024 - Present",
             goal: "Continued Expansion",
             contents:
               "Egg Bucket is committed to capturing more cities and delivering top-quality products to its customers across the region.",
