68d7d8c920d7dffe0c7e13672c8302dcd99c1fb4
sarojkum822 <sarojkum822@gmail.com>
2025-02-11 02:41:38 +0530

    update_referesh5

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '127.0.0.1', // Bind the server to 127.0.0.1
-    port: 5173,        // Optional: Specify the port (default is 5173)
-  },
-});
+})
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '127.0.0.1', // Bind the server to 127.0.0.1
+//     port: 5173,        // Optional: Specify the port (default is 5173)
+//   },
+// });
